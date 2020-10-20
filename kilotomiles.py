c36b2c22b247e50d0ca37a122c62fb06d861b582
Sailesh Pandey <saileshpandey007007@gmail.com>
2020-10-20 13:45:36 +0545

    Convert Kilometers to Miles using python

---
+++ kilotomiles.py
@@ -0,0 +1,11 @@
+#SAILESH PANDEY
+
+#	Python Program to Convert Kilometers to Miles
+#1 kilometers =0.621371192 miles
+
+kilometers = 5
+
+val = 0.621371 #std value
+
+miles = kilometers * val
+print(' The result is : %0.2f kilometers is equal to %0.2f miles' %(kilometers,miles))
