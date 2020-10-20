9f655aff745b3a031cceaf85cabd07ca43b72b2f
Sailesh Pandey <saileshpandey007007@gmail.com>
2020-10-20 13:47:44 +0545

    sort numbers in ascending and descending order using python

---
+++ number in ascending.py
@@ -0,0 +1,14 @@
+#SAILESH PANDEY
+
+#Python Program to sort numbers in ascending and descending order 
+num = [7,19,8,4,22,0,66] #input
+
+num.sort() #sort function to sort
+
+print('the arranged numbers are')
+
+print (num)
+
+
+
+
