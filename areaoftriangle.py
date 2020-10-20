8d9fec02ff8ba789b366614ca78a4c9f7590dc5b
Sailesh Pandey <saileshpandey007007@gmail.com>
2020-10-20 13:40:09 +0545

    Calculate the Area of a Triangle

---
+++ areaoftriangle.py
@@ -0,0 +1,15 @@
+
+#SAILESH PANDEY
+
+#Python Program to Calculate the Area of a Triangle
+a = 2
+
+b = 3
+
+c = 4
+
+s = (a + b + c) / 2
+
+A = (s*(s-a)*(s-b)*(s-c)) ** 0.5
+
+print('The area  is %0.1f' %A)
