9b51179cc8cfd265cf7c77da28f4c280be031838
Sailesh Pandey <saileshpandey007007@gmail.com>
2020-10-20 13:45:57 +0545

    Swap Two Variables using python

---
+++ swap.py
@@ -0,0 +1,15 @@
+#SAILESH PANDEY
+
+#	Python Program to Swap Two Variables
+a=25
+b=50
+
+print('The value of a before swapping: {}'.format(a))
+print('The value of b before swapping: {}'.format(b))
+
+temp = a
+a= b
+b= temp
+
+print('The value of a after swapping: {}'.format(a))
+print('The value of b after swapping: {}'.format(b))
