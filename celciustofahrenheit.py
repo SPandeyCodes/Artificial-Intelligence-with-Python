87d2e5d20671faa3956af670bcbcdd3d1b6c1f19
Sailesh Pandey <saileshpandey007007@gmail.com>
2020-10-20 13:46:28 +0545

    Convert Celsius To Fahrenheit using python

---
+++ celciustofahrenheit.py
@@ -0,0 +1,9 @@
+#SAILESH PANDEY
+#	Python Program to Convert Celsius To Fahrenheit
+
+# 1 degree Celsius = 33.8 degrees Fahrenheit
+cel = 37.8
+
+fah= (cel * 1.8) + 32  #std formula
+
+print(' The result is : %0.1f in degree Celsius is equal to %0.1f in degree Fahrenheit' %(cel,fah))
